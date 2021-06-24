0b2e0722f374016afe0ff250c662077cca80911a
jbm <john.b.mains@gmail.com>
2021-06-24 19:41:54 -0400

    add ci to testcov, clang fix

--- include/cbr_control/mpc/dltv_ocp_solver.hpp
+++ include/cbr_control/mpc/dltv_ocp_solver.hpp
@@ -534,7 +534,7 @@ public:
     // Create U
     const Eigen::Map<const Eigen::Matrix<double, nu, nPts - 1>> mU(sol_.z.data() + nX);
 
-    sol_.u.template col(nPts - 1) = mU.template rightCols<1>();
+    sol_.u col(nPts - 1) = mU.template rightCols<1>();
     sol_.u.template leftCols<nPts - 1>() = mU;
 
     return sol_;
